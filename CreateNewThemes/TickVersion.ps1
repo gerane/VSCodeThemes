0ec728cc13fcdfe22fd9151c5a72d90ceacb1568
Brandon Padgett <gerane@gmail.com>
2019-10-16 12:55:18 -0500

    fixed #66, fixed #67, fixed #68, fixed #69, fixed #70, fixed #71, fixed #72, fixed #73, fixed #74, fixed #75, fixed #76, fixed #77, fixed #78

--- CreateNewThemes/TickVersion.ps1
+++ CreateNewThemes/TickVersion.ps1
@@ -3,11 +3,8 @@ param()
 
 $Themes = Get-ChildItem $PSScriptRoot\..\*readme.md -Recurse
 
-foreach ($Theme in $Themes[0])
+foreach ($Theme in $Themes)
 {
-    Write-Verbose $Theme.FullName
-    (Get-Content $Theme.FullName | Where-Object {$_ -notmatch 'A theme based on the'}) | Out-File $Theme.FullName -Encoding ascii
-
     $ThemeDir = $Theme.FullName | Split-Path -Parent
     $Json = Join-Path $ThemeDir -ChildPath package.json
 
@@ -18,8 +15,9 @@ foreach ($Theme in $Themes[0])
     $JsonObj.Version = $NewVersion
     $JsonObj | ConvertTo-Json -Depth 3 | Out-File -FilePath $Json -Encoding ascii
 
-    # Push-Location $ThemeDir
-    # vsce package
-    # vsce publish
-    # Pop-Location
+
+    Push-Location $ThemeDir
+    vsce package
+    vsce publish
+    Pop-Location
 }
--- CreateNewThemes/UpdateThemes.ps1
+++ CreateNewThemes/UpdateThemes.ps1
@@ -5,7 +5,7 @@ DynamicParam
     # Set the dynamic parameters' name
     $ParameterName = 'Name'
 
-    # Create the dictionary 
+    # Create the dictionary
     $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
 
     # Create the collection of attributes
@@ -18,7 +18,7 @@ DynamicParam
     # Add the attributes to the attributes collection
     $AttributeCollection.Add($ParameterAttribute)
 
-    # Generate and set the ValidateSet 
+    # Generate and set the ValidateSet
     $arrSet = Get-ChildItem -Path "$PSScriptRoot\gerane.*" -Directory | Select-Object -ExpandProperty Name | ForEach-Object { $_ -replace "gerane.Theme-","" }
     $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
 
@@ -41,27 +41,27 @@ begin
 }
 
 process
-{   
+{
     $ThemeDir = $NameDir.FullName
 
     $ThemeName = $NameDir.Name
-    
+
     $Json = Join-Path $ThemeDir -ChildPath package.json
-    
+
     Remove-Item -Path "$ThemeDir\icon.svg" -Force -ErrorAction SilentlyContinue
-    
+
     ## Update Package.json
     $JsonFile = Get-Content $Json
     $Version = ($JsonFile | select-string '"version": ".*,').matches.Value[-3]
     $VersionOld = ($JsonFile | select-string '"version": ".*,').matches.Value
     $NewVersion = [convert]::ToInt32($Version, 8) + 1
-    
+
     $JsonFile1 = $jsonfile.replace('icon.svg','icon.png')
-    $JsonFile2 = $JsonFile1.replace($VersionOld,"`"version`": `"0.0.$NewVersion`"`,")                
+    $JsonFile2 = $JsonFile1.replace($VersionOld,"`"version`": `"0.0.$NewVersion`"`,")
     $JsonFile2 | Out-File -FilePath $Json -Encoding ascii
 
     $FixedName = $Name -replace "_",""
-    
+
     ## Update Readme
     $ReadmeNew = @"
 
@@ -77,21 +77,21 @@ process
 
     $Readmefile = Join-Path $NameDir.FullName -ChildPath Readme.md
     Add-Content -Path $Readmefile -Value $ReadmeNew
-    
-    
+
+
     ## Take Cropped Screenshot
     Function Get-Screenshot
     {
         [CmdletBinding()]
         param
         (
-            [Drawing.Rectangle]$bounds, 
+            [Drawing.Rectangle]$bounds,
             $path
         )
-    
+
         & code-insiders
         Start-Sleep -Seconds 2
-    
+
         [Reflection.Assembly]::LoadWithPartialName("System.Drawing")
         $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
         $graphics = [Drawing.Graphics]::FromImage($bmp)
@@ -100,25 +100,25 @@ process
         $graphics.Dispose()
         $bmp.Dispose()
     }
-    
+
     ## Open VSCode to Script File
     & code-insiders
     Start-Sleep -Seconds 2
-    
+
     ## Screenshot Code
     $Path = "$ThemeDir\screenshot.png"
     $bounds = [Drawing.Rectangle]::FromLTRB(365, 79, 1183, 542)
     Get-Screenshot -Bounds $bounds -Path $Path
-    
+
     ## Screenshot Icon
     $Path = "$ThemeDir\icon.png"
     $bounds = [Drawing.Rectangle]::FromLTRB(406, 90, 534, 218)
     Get-Screenshot -Bounds $bounds -Path $Path
-    
+
     # Copy Items to Repo
     $Repo = Get-GithubRepoDir -Name VSCodeThemes
     $RepoTheme = Join-Path -Path $Repo -ChildPath $ThemeName
-    
+
     Copy-Item -Path "$ThemeDir\*" -Destination $RepoTheme -Recurse -Force
     Remove-Item -Path "$RepoTheme\icon.svg" -Force -ErrorAction SilentlyContinue
 
@@ -141,7 +141,7 @@ process
     #& mspaint.exe "$RepoTheme\screenshot.png"
     #& atom "$RepoTheme\package.json"
     #& atom "$RepoTheme\readme.md"
-    #& explorer.exe "$Repo\$ThemeName"    
+    #& explorer.exe "$Repo\$ThemeName"
 }
 
 end
