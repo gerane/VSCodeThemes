651c2465694499aff552c9ffc22277179cec0f66
Brandon Padgett <gerane@gmail.com>
2019-10-15 22:33:33 -0500

    fix readme encoding

--- CreateNewThemes/RemoveSublime.ps1
+++ CreateNewThemes/RemoveSublime.ps1
@@ -5,7 +5,8 @@ $Themes = Get-ChildItem $PSScriptRoot\..\*readme.md -Recurse
 
 foreach ($Theme in $Themes)
 {
-    (Get-Content $Theme.FullName | Where-Object {$_ -notmatch 'A theme based on the'}) | Out-File $Theme.FullName
+    Write-Verbose $Theme.FullName
+    (Get-Content $Theme.FullName | Where-Object {$_ -notmatch 'A theme based on the'}) | Out-File $Theme.FullName -Encoding ascii
 
     $ThemeDir = $Theme.FullName | Split-Path -Parent
     $Json = Join-Path $ThemeDir -ChildPath package.json
@@ -16,4 +17,9 @@ foreach ($Theme in $Themes)
     $NewVersion = "$($Version.Major).$($Version.Minor).$($Version.Build + 1)"
     $JsonObj.Version = $NewVersion
     $JsonObj | ConvertTo-Json | Out-File -FilePath $Json -Encoding ascii
+
+    Push-Location $ThemeDir
+    vsce package
+    vsce publish
+    Pop-Location
 }
